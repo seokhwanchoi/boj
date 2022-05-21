6a6856b6a636a1f73d94909309fdfffda72dc716
seokhwanchoi <bmsk20@gmail.com>
2022-05-21 19:39:02 +0900

    dd

--- src/백준/공유기설치_2110.java
+++ src/백준/공유기설치_2110.java
@@ -5,5 +5,7 @@ import java.util.*;
 public class 공유기설치_2110 {
 	public static void main(String[] args)throws Exception {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+	
 	}
 }
