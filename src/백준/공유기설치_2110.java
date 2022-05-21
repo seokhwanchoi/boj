9b886209c3c8a6aa328f2b0cfa425973dc8fd518
seokhwanchoi <bmsk20@gmail.com>
2022-05-21 21:57:37 +0900

    a

--- src/백준/공유기설치_2110.java
+++ src/백준/공유기설치_2110.java
@@ -3,10 +3,49 @@ package 백준;
 import java.io.*;
 import java.util.*;
 public class 공유기설치_2110 {
-	static int N,C;
+	static int N,C,house[];
 	public static void main(String[] args)throws Exception {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		N=Integer.parseInt(st.nextToken());
+		C=Integer.parseInt(st.nextToken());
+		
+		house = new int[N];
+		
+		for(int i=0;i<N;++i)
+			house[i]= Integer.parseInt(br.readLine());
+		
+		Arrays.sort(house);
+		
+		int start = 1;
+		int end = house[N-1]-house[0]+1;
+		
+		while(start<end) {
+			int mid = (start + end)/2;
+			
+			if(solve(mid)<C)
+				end = mid;
+			else
+				start = mid + 1;
+		}
+		
+		System.out.println(start -1);
 	
+	}
+	static int solve(int dist) {
+		int cnt = 1;
+		int last = house[0];
+		
+		for(int i=1;i<house.length;++i) {
+			int curr = house[i];
+			
+			if(curr-last >= dist) {
+				cnt++;
+				last=curr;
+				
+			}
+		}
+		return cnt;
 	}
 }
