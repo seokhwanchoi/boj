912d8aca50da7363b57df35bf9c735f2df06c4e0
seokhwanchoi <bmsk20@gmail.com>
2022-05-21 19:46:31 +0900

    aa

--- src/백준/공유기설치_2110.java
+++ src/백준/공유기설치_2110.java
@@ -3,6 +3,7 @@ package 백준;
 import java.io.*;
 import java.util.*;
 public class 공유기설치_2110 {
+	static int N,C;
 	public static void main(String[] args)throws Exception {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
