5de95034fbdf107d017f333c4b0791e1dcee3dec
seokhwanchoi <bmsk20@gmail.com>
2022-05-21 19:33:12 +0900

    bbb

--- src/백준/공유기설치_2110.java
+++ src/백준/공유기설치_2110.java
@@ -1,7 +1,9 @@
 package 백준;
 
+import java.io.*;
+import java.util.*;
 public class 공유기설치_2110 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 	}
 }
